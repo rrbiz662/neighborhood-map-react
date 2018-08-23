d586bd61e8b58490f3ccec09fe0ee0a67dbb9d0b
rrbiz662 <rrbiz662@gmail.com>
2018-08-22 18:55:14 -0700

    feat: Add sidebar toggling capability
    Add toggleSidebar method to handle Menu button clicks and display sidebar accordingly.

--- neighborhoodmap/src/Navbar.js
+++ neighborhoodmap/src/Navbar.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import {Navbar as Navigationbar, Nav, NavItem} from 'react-bootstrap';
 
+//let sidebar = document.getElementById("sidebar");
+
 class NavBar extends React.Component{
+    toggleSidebar = () => {
+        console.log("Menu clicked");
+        // width = sidebar.css("width");
+
+        // if(width === "0px")
+        //     sidebar.css("width", "250px");
+        // else
+        //     sidebar.css("width", "0");
+    }
+
     render(){
         return (
             <Navigationbar>
@@ -11,7 +23,7 @@ class NavBar extends React.Component{
                     </Navigationbar.Brand>
                 </Navigationbar.Header>
                 <Nav pullRight>
-                    <NavItem>
+                    <NavItem onClick={this.toggleSidebar}>
                         Menu
                     </NavItem>
                 </Nav>

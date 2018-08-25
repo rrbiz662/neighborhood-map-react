3cfd0b7f500989f8c4e997f1651b643fc9ee5230
rrbiz662 <rrbiz662@gmail.com>
2018-08-25 12:57:05 -0700

    feat: Add form reset
    Clear form and map when the clear button is clicked.

--- neighborhoodmap/src/LocationForm.js
+++ neighborhoodmap/src/LocationForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from  "react-dom"
 import PropTypes from "prop-types";
 
 const API_KEY = "zboWotd5QomCFouN96e-YRf7deALxng825rC-GpXWbeoTGZmaOYtCyl6U9eMOEJd09KNTzo6H12cbxoQb_jetLKrD_NHDf1fqVfYmAlEgvG6TZdx2qvNPiVmLWvqWnYx";
@@ -15,60 +16,67 @@ class LocationForm extends React.Component{
         inputValue: "",
     }
 
-    getBusinesses = (address) => {
-        const config = {
-            headers: {
-                "Authorization": "Bearer " + API_KEY
-            }
-        };
+    baseState = this.state;
 
-        let params = {
-                term: "",
-                location: address,
-                limit: LIMIT,
-                radius: RADIUS
+    getBusinesses = (address) => {
+        if(address === "")
+            this.props.initLists([], [], []);
+        else{
+
+            const config = {
+                headers: {
+                    "Authorization": "Bearer " + API_KEY
+                }
             };
 
-        // Add search params to URL.
-        let url = new URL("https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search");
-        Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+            let params = {
+                    term: "",
+                    location: address,
+                    limit: LIMIT,
+                    radius: RADIUS
+                };
 
-        fetch(url, config
-        ).then((res) => {
-            return res.json();
-        }).then((json) => {
-            let businesses = [];
-            let filters = [];
+            // Add search params to URL.
+            let url = new URL("https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search");
+            Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
 
-            for (const business of json.businesses) {
-                businesses.push({
-                    id: business.id,
-                    name: business.name,
-                    img: business.image_url,
-                    phone: business.display_phone,
-                    address: {
-                        street: business.location.address1,
-                        city: business.location.city,
-                        state: business.location.state,
-                        zipCode: business.location.zip_code
-                    },
-                    latitude: business.coordinates.latitude,
-                    longitude: business.coordinates.longitude,
-                    categories: business.categories,
-                });
+            fetch(url, config
+            ).then((res) => {
+                return res.json();
+            }).then((json) => {
+                let businesses = [];
+                let filters = [];
 
-                business.categories.forEach((category) => {
-                    if(filters.indexOf(category.title) === -1)
-                        filters.push(category.title);
-                });
-            }
+                for (const business of json.businesses) {
+                    businesses.push({
+                        id: business.id,
+                        name: business.name,
+                        img: business.image_url,
+                        phone: business.display_phone,
+                        address: {
+                            street: business.location.address1,
+                            city: business.location.city,
+                            state: business.location.state,
+                            zipCode: business.location.zip_code
+                        },
+                        latitude: business.coordinates.latitude,
+                        longitude: business.coordinates.longitude,
+                        categories: business.categories,
+                    });
 
-            let filteredBusinesses = businesses.slice(0);
+                    business.categories.forEach((category) => {
+                        if(filters.indexOf(category.title) === -1)
+                            filters.push(category.title);
+                    });
+                }
 
-            this.props.initLists(businesses, filters, filteredBusinesses);
-        }).catch((e) => {
-            console.log("Error" + e);
-        });
+                let filteredBusinesses = businesses.slice(0);
+
+                this.props.initLists(businesses, filters, filteredBusinesses);
+            }).catch((e) => {
+                console.log("Error" + e);
+            });
+        }
     }
 
     handleSubmit = (event) => {
@@ -77,8 +85,17 @@ class LocationForm extends React.Component{
         event.preventDefault();
     }
 
-    handleClear = (event) => {
+    handleClear = () => {
+        const node = ReactDOM.findDOMNode(this);
+        const input = node.getElementsByTagName("input")[0];
 
+        input.value = "";
+
+        this.props.updateLocation(input.value);
+        this.getBusinesses(input.value);
+        this.setState({
+            inputValue: input.value
+        });
     }
 
     handleChange = (event) => {
@@ -94,7 +111,7 @@ class LocationForm extends React.Component{
                 <input type="text" name="location" id="location" className="form-control" placeholder="Austin, TX" onChange={this.handleChange}>
                 </input>
                 <button id="submit-btn" className="btn btn-light mt-3" type="submit">Search</button>
-                <button id="clear-btn" className="btn btn-light mt-3" type="button">Clear</button>
+                <button id="clear-btn" className="btn btn-light mt-3" type="button" onClick={this.handleClear}>Clear</button>
             </form>
         );
     }
